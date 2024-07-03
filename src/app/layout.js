2649533af69d9b4eb62be98e3896d8edd885e9bb
Rijwanul Haque Roman <rizwanul210058@diit.edu.bd>
2024-07-04 03:20:05 +0600

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,16 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "rizwanul.dev",
   description:
-    "Rizwanul is an enthusiastic developer who can convert thoughts into reality with code. By tirelessly seeking new ideas, he merge creativity with practicality when creating amazing websites that are eye candy for the users.",
+    "Rizwanul is an enthusiastic developer who can convert thoughts into reality with code. By tirelessly seeking new ideas, he merges creativity with practicality when creating amazing websites that are eye candy for the users.",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+  },
 };
 
 export default function RootLayout({ children }) {
